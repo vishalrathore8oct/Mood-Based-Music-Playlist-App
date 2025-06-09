3eceb79c53814a598db2da8574731c7404fda46f
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-06-09 10:56:45 +0530

    setup react-router-dom with some testing pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Explore from "./pages/Explore"
+import LikedSongs from "./pages/LikedSongs"
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world Ji!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/explore" element={<Explore/>}></Route>
+        <Route path="/liked-songs" element={<LikedSongs/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
